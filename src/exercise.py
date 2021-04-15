d7421de4eaff02756eca972feb63249697939410
pmatyus <>
2021-04-15 08:30:11 +0000

    Exercise solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
     #write your code below this line
+    number_of_days = int(input('How many days would you like to convert to seconds?'))
+    print(number_of_days * 24 * 60 * 60)
 
 if __name__ == '__main__':
     main()
